7e7dfd5f905bc72610e846a93f95bc2531308eac
Noah <89213023+chifouu65@users.noreply.github.com>
2024-05-13 11:50:34 +0000

    fix: (login)

--- back/src/controllers/users.controller.js
+++ back/src/controllers/users.controller.js
@@ -46,35 +46,37 @@ exports.signUp = async (req, res) => {
 
 exports.signIn = async (req, res) => {
   try {
-  let user = User.findOne({ username: req.body.username.toLowerCase() })
-  if (user) {
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (isMatch && !err) {
-        // if user is found and password is right create a token
-        const token = jwt.sign(user.toJSON(), process.env.SECRET_TOKEN);
-        // return the information including token as JSON
-        return res.status(200).json({
-          message: "Authentification réussie",
-          data: {
-            id: user["_id"],
-            token: token,
-            username: user["username"],
-          },
-          isSuccess: true,
+    User.findOne({ username: req.body.username.toLowerCase() }).then((user) => {
+      if (user) {
+        user.comparePassword(req.body.password, (err, isMatch) => {
+          if (isMatch && !err) {
+            // if user is found and password is right create a token
+            const token = jwt.sign(user.toJSON(), process.env.SECRET_TOKEN);
+            // return the information including token as JSON
+            return res.status(200).json({
+              message: "Authentification réussie",
+              data: {
+                id: user["_id"],
+                token: token,
+                username: user["username"],
+              },
+              isSuccess: true,
+            });
+          } else {
+            res.status(500).json({
+              message: "Échec authentification, veuillez réessayer",
+              isSuccess: false,
+            });
+          }
         });
-      } else {
-        res.status(500).json({
-          message: "Échec authentification, veuillez réessayer",
-          isSuccess: false,
-        });
-      }
-    });
+ 
   } else {
     return res
       .status(401)
       .json({ message: "Aucun user correspondant", isSuccess: false });
   }
- 
+  
+    })
   } catch (error) {
     console.log(error)
     return res.status(500).json({ message: error.message, isSuccess: false });
